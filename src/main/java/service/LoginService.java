c0cdbde522c05d1346dc80e25a54b19dddeb43b4
KIM_L <KIM_L@192.168.1.7>
2018-04-09 23:11:45 +0900

    modify4-09

---
+++ src/main/java/service/LoginService.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("LoginService")
+public class LoginService {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int login(Map<String, Object> map) {
+		
+		return sqlSession.selectOne("login", map);
+	}
+}
